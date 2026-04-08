22cbc789c5ce4c3ff7592e2e82231c681a57a535
ankit4565 <ankitbowade3731@gmail.com>
2026-04-08 17:01:02 +0530

    key_fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 // config/db.js
 
 const mongoose = require("mongoose");
+const User = require("../models/users");
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await User.syncIndexes();
     } catch (error) {
         console.log("DB Error:", error.message);
         process.exit(1);
